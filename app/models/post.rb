74041a00b35d605e14f17523e3b500a96e1db455
dut-a <d.dut@live.com>
2019-12-29 05:26:01 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+    # Enforcing custom validator
+    validate :title_must_be_clickbaity
+
+    validates :title, { :presence => true }
+    validates :content, { length: { :minimum => 250 } }
+    validates :summary, { length: { :maximum => 250 } }
+    validates :category, { inclusion: { in: %w(Fiction Non-Fiction) } }
+ 
+    # Custom validator
+    def title_must_be_clickbaity
+        expected_present = [ /Won't Believe/i, /Secret/i, /Top [0-9]+/i, /Guess/i ]
+
+        if expected_present.none? { |w| w.match(title) }
+            errors.add(:title, "isn't clickbait-y.")
+        end
+    end
+
 end
